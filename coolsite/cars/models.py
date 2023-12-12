de3e2a6d1b4ab6333621637b9d44c242182e5c09
Admin <nurbol1314@gmail.com>
2023-12-12 16:36:29 +0500

    добавлена в гит

--- coolsite/cars/models.py
+++ coolsite/cars/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Cars(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Известные авто'
+        verbose_name_plural = 'Известные авто'
+        ordering = ['time_create', 'title']
+
+class Category(models.Model):
+        name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
+
+        def __str__(self):
+            return self.name
+
+        def get_absolute_url(self):
+            return reverse('category', kwargs={'cat_id': self.pk})
+
+        class Meta:
+            verbose_name = 'Категория'
+            verbose_name_plural = 'Категории'
+            ordering = ['id']
--- coolsite/cars/views.py
+++ coolsite/cars/views.py
@@ -1,5 +1,58 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import *
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
+
+
 def index(request):
-    return HttpResponse("Страница приложения cars.")
+    posts = Cars.objects.all()
+    cats = Category.objects.all()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': 0,
+    }
+
+    return render(request, 'cars/index.html', context=context)
+
+def about(request):
+    return render(request, 'cars/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+
+def show_category(request, cat_id):
+    posts = Cars.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отоброжени',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'women/index.html', context=context)
