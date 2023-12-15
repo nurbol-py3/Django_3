7289d017519e0459fd98ba6b585712d56b184e3b
Admin <nurbol1314@gmail.com>
2023-12-15 22:03:15 +0500

    Add file

--- coolsite/cars/admin.py
+++ coolsite/cars/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 
+
 class CarsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
     list_display_links = ('id', 'title')
@@ -8,11 +9,12 @@ class CarsAdmin(admin.ModelAdmin):
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
 
+
 admin.site.register(Cars, CarsAdmin)
 admin.site.register(Category, CategoryAdmin)
-
